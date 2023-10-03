c1c2bd953073b6b5242daf7ae575be4a28a11db0
Md Asif Alam <mdasifalam910@gmail.com>
2023-10-03 13:28:18 +0530

    Template Literal explained

---
+++ Template-literal.js
@@ -0,0 +1,24 @@
+//es5
+// console.log("My name is Asif." + " My age is 26");
+
+//es6
+// console.log(`My name is Asif. My age is 26`);
+
+// let firstName = "Asif";
+// let lastName = "Alam";
+// console.log(
+//   "My first name is " + firstName + ". My last name is " + lastName + "."
+// );
+
+//es6
+// console.log(`My first name is ${firstName}. My last name is ${lastName}`);
+
+//es5
+let a = 20;
+let b = 30;
+//es5
+// console.log("Fifty is " + (a + b) + "and\nnot " + (2 * a + b) + ".");
+
+//es6
+console.log(`Fifty is ${a + b} and 
+not ${2 * a + b}.`);
