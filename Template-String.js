56f1ea30b96bfe9577bca8ee55b789a86fd1b9c8
Md Asif Alam <mdasifalam910@gmail.com>
2023-10-03 14:23:22 +0530

    Template String methods explained

---
+++ Template-String.js
@@ -0,0 +1,13 @@
+let firstName = "Asif";
+
+// console.log(`${firstName}`.startsWith("A"));
+// console.log(`${firstName}`.endsWith("f"));
+// console.log(`${firstName}`.includes("As"));
+// console.log(`${firstName} `.repeat(10));
+
+const fname = `${firstName} `;
+
+console.log(fname.startsWith("A"));
+console.log(fname.endsWith("f"));
+console.log(fname.includes("A's"));
+console.log(fname.repeat(10));
