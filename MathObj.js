0da98c577543e4a6ee918df76950ec75ab5bb38f
Md Asif Alam <mdasifalam910@gmail.com>
2023-10-05 09:32:41 +0530

    Math sign and trunc method explained

---
+++ MathObj.js
@@ -0,0 +1,24 @@
+let val1 = 5;
+let val2 = -5;
+let val3 = 5.546;
+let val4 = -5.546;
+
+let val5 = 0;
+let val6 = -0;
+console.log("====== Math sign =========");
+console.log(Math.sign(val1));
+console.log(Math.sign(val2));
+console.log(Math.sign(val3));
+console.log(Math.sign(val4));
+console.log(Math.sign(val5));
+console.log(Math.sign(val6));
+
+console.log("====== Math trunc =========");
+console.log(Math.trunc(val1));
+console.log(Math.trunc(val4));
+console.log(Math.trunc(val3));
+
+console.log("====== Math floor =========");
+console.log(Math.floor(val1));
+console.log(Math.floor(val4));
+console.log(Math.floor(val3));
